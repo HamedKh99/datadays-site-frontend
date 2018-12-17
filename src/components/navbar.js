431a5edf2568f7cc079112dee6743dbbc4f36305
HamedKh99 <goo.hamedacc@gmail.com>
2018-12-17 14:15:00 +0330

    implementing topbar

---
+++ src/components/navbar.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import { withStyles } from '@material-ui/core';
+import '../App.css'
+
+const styles = theme => ({
+    navbar : {
+        width : '25%',
+        overflow : 'hidden',
+        display : 'flex',
+        justifyContent : 'space-around',
+        
+    },
+    navbarItem : {
+        fontFamily : 'Parastoo',
+        textAlign : 'center',
+        padding : '5px 10px',
+        borderRadius : '10px',
+        textDecoration : 'none',
+        color : 'white',
+        '&:hover' : {
+            backgroundColor : theme.palette.primary.light
+        }
+    }
+});
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {classes} = this.props
+        return (
+            <nav className={classes.navbar}>
+                <a href='/' className={classes.navbarItem}>صفحه اصلی</a>
+                <a href='/' className={classes.navbarItem}>بلاگ</a>
+                <a href='/' className={classes.navbarItem}>تماس با ما</a>
+            </nav>
+        )
+    }
+}
+
+export default withStyles(styles)(Navbar)
---
+++ src/components/topbar.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { AppBar, withStyles, Toolbar, IconButton, Typography, InputBase } from '@material-ui/core';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import '../App.css'
+import Navbar from './navbar'
+
+const styles = theme => ({
+    root: {
+      width: '100%',
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing.unit,
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing.unit * 9,
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      fontFamily : 'Parastoo',  
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 10,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  });
+
+class TopBar extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                    <MenuIcon />
+                </IconButton>
+                <Navbar/>
+                <div className={classes.grow} />
+                <div className={classes.search}>
+                    <InputBase
+                    placeholder="جستجو..."
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    }}
+                    />
+                </div>
+                </Toolbar>
+            </AppBar>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(TopBar)
